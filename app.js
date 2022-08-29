0329b9af80fc579e18ddecb14463a069a0922068
root <root@ip-10-0-118-206.eu-central-1.compute.internal>
2022-08-29 12:42:40 +0000

    Hannover test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello E.ON Digital World!'));
+app.get('/', (req, res) => res.send('Hello E.ON Digital World at Hannover (Germany)!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
